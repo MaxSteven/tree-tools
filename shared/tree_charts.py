b66324a2a2a13707af8b6d8382e9f68d8e6ef97f
Fredrik Averpil <fredrikaverpil@gmail.com>
2016-03-08 15:15:07 +0100

    Tweaked legend

--- shared/tree_charts.py
+++ shared/tree_charts.py
@@ -1,5 +1,4 @@
 import os
-import operator
 
 import pygal
 
@@ -7,6 +6,7 @@ import tree_functions
 
 # Functions
 functions = tree_functions.TreeFunctions()
+
 
 class Charts(object):
     """docstring for Charts"""
@@ -42,7 +42,7 @@ class Charts(object):
         if div == 0:
             div = 1  # prevent division if zero
 
-        chart = pygal.Bar()
+        chart = pygal.Bar(truncate_legend=35)
         chart.title = 'Size in ' + unit
         if isinstance(index, list):
             # Index has been sorted
@@ -51,14 +51,15 @@ class Charts(object):
                 value = item[1]
                 name = os.path.basename(key)
                 values = [value['b'] / div]
-                chart.add(name, values)
+                nice_size = functions.nice_number(b=value['b'])
+                chart.add(nice_size + ' ' + name, values)
         elif isinstance(index, dict):
             # Index is unsorted
             for key, value in index.iteritems():
                 name = os.path.basename(key)
                 values = [value['b'] / div]
-                chart.add(name, values)
-
+                nice_size = functions.nice_number(b=value['b'])
+                chart.add(nice_size + ' ' + name, values)
         chart.render_to_file('tree_leafsize_bar_chart.svg')
 
     def pie_chart(self, index, unit, div):
@@ -67,7 +68,7 @@ class Charts(object):
         if div == 0:
             div = 1  # prevent division if zero
 
-        chart = pygal.Pie()
+        chart = pygal.Pie(truncate_legend=35)
         chart.title = 'Size in ' + unit
 
         if isinstance(index, list):
@@ -77,12 +78,13 @@ class Charts(object):
                 value = item[1]
                 name = os.path.basename(key)
                 values = [value['b'] / div]
-                chart.add(name, values)
+                nice_size = functions.nice_number(b=value['b'])
+                chart.add(nice_size + ' ' + name, values)
         elif isinstance(index, dict):
             # Index is unsorted
             for key, value in index.iteritems():
                 name = os.path.basename(key)
                 values = [value['b'] / div]
-                chart.add(name, values)
-
+                nice_size = functions.nice_number(b=value['b'])
+                chart.add(nice_size + ' ' + name, values)
         chart.render_to_file('tree_leafsize_pie_chart.svg')
